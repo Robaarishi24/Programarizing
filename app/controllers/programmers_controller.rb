933ec1ff1acb7b246c61e45f1dbdfa913c9b9a5c
robaarishi24 <robaarishi@hotmail.com>
2020-04-05 04:56:35 +0300

    edit a programmer

--- app/controllers/programmers_controller.rb
+++ app/controllers/programmers_controller.rb
@@ -13,4 +13,12 @@ class ProgrammersController < ApplicationController
         Programmer.create(params.require(:programmer).permit(:first_name, :last_name, :phone, :email))
     redirect_to programmers_path
     end
+    def edit 
+        @programmer = Programmer.find(params[:id])
+    end
+    def update
+        programmer = Programmer.find(params[:id])
+        programmer.update(params.require(:programmer).permit(:first_name, :last_name, :phone, :email))
+        redirect_to programmers_path
+      end
 end
