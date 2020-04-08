d3b52cedd087f29fd79b87882727bf88df80c35d
robaarishi24 <robaarishi@hotmail.com>
2020-04-08 16:23:31 +0300

    fixed routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'sign_up' , to: 'pages#sign_up' 
-  get 'sign_in' , to: 'pages#sign_in'
+  get '/users/sign_in' , to: 'devise/sessions#new' 
+  get '/users/sign_up' , to: 'devise/registrations#new' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'programmers#index'
   resources :programmers , :projects
-
   # get 'programmers', to: 'programmers#index'
   # get 'programmers/:id', to: 'programmers#show', as: :programmer
 end
